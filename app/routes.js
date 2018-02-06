2b5fd9edc88ed2806669919a095953ca9a48ccd6
Mateo Silguero <mfsilguero@gmail.com>
2018-02-06 16:24:23 -0300

    Se hacen mas especificaciones en la documentacion

--- app/routes.js
+++ app/routes.js
@@ -13,7 +13,46 @@ const routes = new Router();
 
 routes.get('/', MetaController.index);
 
+
+/**
+* @apiDefine HeaderAuthorization
+* @apiHeader {String} Authorization Bearer [token]
+* @apiHeaderExample {json} Header-Example:
+*     {
+*       "Authorization": "eyJh..."
+*     }
+*
+* @apiErrorExample {json} Token error
+ *    HTTP/1.1 401 
+ *    {
+ *      "Unauthorized"
+ *    }
+*/
+
+
 // Authentication
+/**
+ * @api {post} /auth/login Authentication
+ * @apiGroup Login
+ * @apiParam {String} username username
+ * @apiParam {String} password password
+ * @apiParamExample {json} Input
+ *    {
+ *      "username": "john perez",
+ *      "password": "password1"
+ *    }
+ * @apiSuccessExample {json} Success
+ *    HTTP/1.1 200
+ *    {
+ *      "token":"eyJh..."
+ *    }
+ * @apiErrorExample {json} Login error
+ *    HTTP/1.1 401 
+ *    {
+ *      "message": "Please verify your credentials"
+ *    }
+ *    HTTP/1.1 500 Internal Server Error
+ */
 routes.post('/auth/login', AuthController.login);
 
 // Users
@@ -26,12 +65,16 @@ routes.post('/auth/login', AuthController.login);
  *      "success":"true", 
  *      [
  *        {
- *         	"firstname": "Jonh",
- *         	"lastname": "Doe"
- *        }
+ *			"updatedAt": "2018-02-06T12:35:59.819Z",
+ *			"createdAt": "2018-02-06T12:35:59.819Z",
+ *			"username": "jamesdean",
+ *			"email": "example@gmail.com",
+ *			"role": "user",
+ *			"id": "5a79a12f2ca0fe10402f7b78"
+ *		  }
  *      ]
  *    }
- * @apiErrorExample {json} Update error
+ * @apiErrorExample {json} Get error
  *    HTTP/1.1 500 Internal Server Error
  */
 routes.get('/users', UsersController.findAll);
@@ -44,24 +87,29 @@ routes.get('/users', UsersController.findAll);
  * @apiParam {String} email User email
  * @apiParam {String} password User password
  * @apiParam {String} role=user User role
+ * @apiUse HeaderAuthorization
  * @apiParamExample {json} Input
  *    {
  *      "firstname": "Jonh",
- *      "lastname": "Doe"
+ *      "lastname": "Doe",
+ *      "username": "johndoe",
+ *      "email": "example@email.com",
+ *      "password": "supersecret"
  *    }
  * @apiSuccessExample {json} Success
  *    HTTP/1.1 204 
  *    {
  *      "success":"true",
  *    }
- * @apiErrorExample {json} Update error
+ * @apiErrorExample {json} Create error
  *    HTTP/1.1 500 Internal Server Error
  */
-routes.post('/users', UsersController.create);
+routes.post('/users', authenticate, UsersController.create);
 /**
  * @api {post} /users/ Get current user
  * @apiGroup Users
-  * @apiSuccessExample {json} Success
+ * @apiUse HeaderAuthorization
+ * @apiSuccessExample {json} Success
  *    HTTP/1.1 204 
  *    {
  *      "success":"true", 
@@ -72,7 +120,7 @@ routes.post('/users', UsersController.create);
  *        }
  *      ]
  *    }
- * @apiErrorExample {json} Update error
+ * @apiErrorExample {json} Get error
  *    HTTP/1.1 500 Internal Server Error
  */
 routes.get('/users/me', authenticate, UsersController.fetch);
@@ -85,6 +133,7 @@ routes.get('/users/me', authenticate, UsersController.fetch);
  * @apiParam {String} email User email
  * @apiParam {String} password User password
  * @apiParam {String} role User role
+ * @apiUse HeaderAuthorization
  * @apiParamExample {json} Input
  *    {
  *      "firstname": "Jonh",
@@ -102,6 +151,7 @@ routes.put('/users/me', authenticate, UsersController.update);
 /**
  * @api {delete} /users/me Delete current user
  * @apiGroup Users
+ * @apiUse HeaderAuthorization
  * @apiParam {String} username User username
  * @apiParamExample {json} Input
  *    {
@@ -113,7 +163,7 @@ routes.put('/users/me', authenticate, UsersController.update);
  *    {
  *      "success":"true",
  *    }
- * @apiErrorExample {json} Update error
+ * @apiErrorExample {json} Delete error
  *    HTTP/1.1 500 Internal Server Error
  */
 routes.delete('/users/me', authenticate, UsersController.delete);
@@ -128,10 +178,14 @@ routes.delete('/users/me', authenticate, UsersController.delete);
  * @apiSuccessExample {json} Success
  *    HTTP/1.1 204 
  *    {
- *      "firstname": "Jonh",
- *      "lastname": "Doe"
+ *      "updatedAt": "2018-02-06T12:35:59.819Z",
+ *      "createdAt": "2018-02-06T12:35:59.819Z",
+ *      "username": "jamesdean",
+ *      "email": "example@gmail.com",
+ *      "role": "user",
+ *      "id": "5a79a12f2ca0fe10402f7b78"
  *    }
- * @apiErrorExample {json} Update error
+ * @apiErrorExample {json} Get error
  *    HTTP/1.1 500 Internal Server Error
  */
 routes.get('/users/:username', UsersController._populate, UsersController.fetch);
@@ -141,17 +195,25 @@ routes.get('/users/:username', UsersController._populate, UsersController.fetch)
  * @api {get} /posts Get all posts
  * @apiGroup Posts
  * @apiSuccessExample {json} Success
- *    HTTP/1.1 204 
- *    {
- *      "text": "text"
- *    }
- * @apiErrorExample {json} Update error
+ *    HTTP/1.1 204
+ *    [
+ *      {
+ *         "_id": ObjectId('5a79a2f33d9a491219b36da4'),
+ *         "updatedAt": ISODate('2018-02-06T12:43:31.443Z'),
+ *         "createdAt": ISODate('2018-02-06T12:43:31.443Z'),
+ *         "text": "Vero maiores magni vel omnis et.",
+ *         "_user": ObjectId('5a79a2f33d9a491219b36da3'),
+ *         "__v": 0
+ *      }
+ *    ]
+ * @apiErrorExample {json} Get error
  *    HTTP/1.1 500 Internal Server Error
  */
 routes.get('/posts', PostsController.search);
 /**
  * @api {post} /posts Create new post
  * @apiGroup Posts
+ * @apiUse HeaderAuthorization
  * @apiParam {String} text Post text
  * @apiParamExample {json} Input
  *    {
@@ -163,31 +225,37 @@ routes.get('/posts', PostsController.search);
  *    {
  *      "success": "true"
  *    }
- * @apiErrorExample {json} Update error
+ * @apiErrorExample {json} Create error
  *    HTTP/1.1 500 Internal Server Error
  */
 routes.post('/posts', authenticate, PostsController.create);
 /**
  * @api {get} /posts/:id Get post by id
  * @apiGroup Posts
+ * @apiUse HeaderAuthorization
  * @apiParam {String} _id Post id
  * @apiParamExample {json} Input
  *    {
- *      "text": "text",
- *      "_id": "_id"	
+ *      "_id": "id"	
  *    }
  * @apiSuccessExample {json} Success
  *    HTTP/1.1 204 
  *    {
- *      "text": "text"	
+ *      "_id": ObjectId('5a79a2f33d9a491219b36da4'),
+ *      "updatedAt": ISODate('2018-02-06T12:43:31.443Z'),
+ *      "createdAt": ISODate('2018-02-06T12:43:31.443Z'),
+ *      "text": "Vero maiores magni vel omnis et.",
+ *      "_user": ObjectId('5a79a2f33d9a491219b36da3'),
+ *      "__v": 0	
  *    }
- * @apiErrorExample {json} Update error
+ * @apiErrorExample {json} Get error
  *    HTTP/1.1 500 Internal Server Error
  */
 routes.get('/posts/:id', PostsController._populate, PostsController.fetch);
 /**
  * @api {delete} /posts/:id Delete post by id
  * @apiGroup Posts
+ * @apiUse HeaderAuthorization
  * @apiParam {String} _id Post id
  * @apiParamExample {json} Input
  *    {
@@ -198,7 +266,7 @@ routes.get('/posts/:id', PostsController._populate, PostsController.fetch);
  *    {
  *      "success": "true"	
  *    }
- * @apiErrorExample {json} Update error
+ * @apiErrorExample {json} Delete error
  *    HTTP/1.1 500 Internal Server Error
  */
 routes.delete('/posts/:id', authenticate, PostsController.delete);
